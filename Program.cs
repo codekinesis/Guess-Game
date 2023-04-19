de7e017fa2b732db01689409a7604b2347306503
Benjamin <9857250+CoDeKiNeSiS@users.noreply.github.com>
2023-04-19 13:21:01 -0700

    Initial check-in of module H3D_GuessGame

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace H3D_GuessGame
+{
+    class GuessGame
+    {
+        public static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int number = rand.Next(1, 101);
+            int guess;
+            int guesses = 0;
+
+            Console.Write("What number am I thinking of? ");
+            guess = Convert.ToInt32(Console.ReadLine());
+            guesses++;
+
+
+            while (guess != number)
+            {
+                Console.WriteLine("Sorry, that's incorrect. Try again.");
+                if (guess < number)
+                {
+                    Console.WriteLine("Too low.");
+                }
+                else 
+                {
+                    Console.WriteLine("Too high.");
+                }
+                guess = Convert.ToInt32(Console.ReadLine());
+                guesses++;
+            }
+
+            Console.WriteLine($"Congratulations! It took you {guesses} tries!");
+        }
+    }
+}
